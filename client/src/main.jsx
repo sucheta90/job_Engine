d33e1f69affbdd02dfe56a495e6f643114810917
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-11-20 20:27:20 -0500

    added new routes, added new directory in client/src/components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,39 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import About from "./pages/About.jsx";
+import Candidate from "./pages/Candidate.jsx";
+import Employer from "./pages/Employer.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
 import "./index.css";
 
-const router = createBrowserRouter;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/candidate",
+        element: <Candidate />,
+      },
+      {
+        path: "/employer",
+        element: <Employer />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
