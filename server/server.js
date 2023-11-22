867f4e037ce2e590e8874d8da70716e300179411
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-11-22 18:07:53 -0500

    created server connection, added watch script to server directory

--- server/server.js
+++ server/server.js
@@ -1,28 +1,67 @@
-// const express = require("express");
-// const path = require("path");
-// const routes = require("./routes");
-// const db = require("./config/dbConnection"); /** establishes the db connection */
+const express = require("express");
+const path = require("path");
+const db = require("./config/dbConnection");
+const routes = require("./routes");
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// // Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(testMiddleware2);
+// app.use(testMiddleware);
+// app.use("/specific", testMiddleware3);
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// app.use(routes);
+// app.get("/hello", all);
+// app.get("/hello2", all2);
+
+// app.get("/specific", all3);
+// app.get("/specific/abc", all4);
+app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+// function testMiddleware(req, res, next) {
+//   console.log("Inside testMiddleware");
+//   next();
 // }
 
-// // app.use(routes);
+// function testMiddleware2(req, res, next) {
+//   console.log("Inside testMiddleware2");
+//   next();
+// }
 
-// // app.listen(PORT, () => {
-// //   console.log("Listening to PORT 3001");
-// // });
+// function testMiddleware3(req, res, next) {
+//   console.log("Inside testMiddleware3");
+//   if (
+//     req.query.name == "su" ||
+//     (req.query.name == "cha" && req.query.goo != "sakto")
+//   ) {
+//     return res.status(400).send("Invalid input");
+//   }
+//   next();
+// }
 
-// db.once("open", () => {
-//   app.listen(PORT, () => {
-//     console.log(`Listening on localhost://${PORT}`);
-//   });
-// });
+// function all(req, res) {
+//   console.log("inside all");
+//   return res.json({ message: "success" });
+// }
+
+// function all2(req, res) {
+//   console.log("inside all2");
+//   return res.json({ message: "success" });
+// }
+
+// function all3(req, res) {
+//   console.log("inside all3");
+//   return res.json({ message: "success" });
+// }
+
+// function all4(req, res) {
+//   console.log("inside all4");
+//   return res.json({ message: "success" });
+// }
