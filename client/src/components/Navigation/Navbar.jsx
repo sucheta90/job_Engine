3793d358cc9f3ba5e38f05611e4a7346b19be74d
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-11-28 17:22:26 -0500

    modified Nav bar

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -4,10 +4,11 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function NavHeader() {
+  // Main Navbar
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">JobFound</Navbar.Brand>
+        <Navbar.Brand href="#home">JobFound(get-a-logo)</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
