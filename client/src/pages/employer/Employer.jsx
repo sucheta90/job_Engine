ed0b5c935b7e3b4d03314f83db3628a9fd9f988e
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-12-04 15:40:27 -0500

    setting up login and signup form for company login

--- client/src/pages/employer/Employer.jsx
+++ client/src/pages/employer/Employer.jsx
@@ -7,10 +7,21 @@ import "./Employer.css";
 export default function Employer() {
   // This flag assumes the use is already a member
   const [isMember, setIsMember] = useState(true);
+  const [userFormData, setUserFormData] = useState({
+    loginEmail: "",
+    loginPassword: "",
+  });
+  const [signupFormData, setSignupFormData] = useState({
+    loginEmail: "",
+    loginPassword: "",
+  });
+  // function switches between form (login & signup)
   const switchForms = (e) => {
     e.preventDefault();
     setIsMember((prevState) => !prevState);
   };
+  //
+  function handleInputChange(e) {}
 
   return (
     <Container fluid id="employer_dashboard">
@@ -21,10 +32,22 @@ export default function Employer() {
             {isMember ? (
               <form action="" id="company-login">
                 <h3>Login</h3>
-                <label htmlFor="">Username</label>
-                <input type="email" placeholder="email" />
-                <label htmlFor="">Password</label>
-                <input type="password" placeholder="password" />
+                <label htmlFor="loginEmail">Username</label>
+                <input
+                  id="loginEmail"
+                  type="email"
+                  placeholder="email"
+                  name="loginEmail"
+                  value=""
+                />
+                <label htmlFor="login-password">Password</label>
+                <input
+                  id="loginPassword"
+                  type="password"
+                  placeholder="password"
+                  name="loginPassword"
+                  value=""
+                />
                 <Button>Log In</Button>
                 <p>
                   Not a member yet?{" "}
@@ -36,18 +59,62 @@ export default function Employer() {
             ) : (
               <form action="" id="company-signup">
                 <h3>Sign up</h3>
-                <label htmlFor="">Company Name</label>
-                <input type="text" placeholder="Company Name" />
-                <label htmlFor="">Address</label>
-                <textarea type="text" placeholder="Company Name" />
-                <label htmlFor="">Point of Contact</label>
-                <input type="text" placeholder="Your Name" />
-                <label htmlFor="">Company Website</label>
-                <input type="text" placeholder="Company Name" />
-                <label htmlFor="">Email</label>
-                <input type="text" placeholder="Company Name" />
-                <label htmlFor="">Password</label>
-                <input type="text" placeholder="Company Name" />
+                <label htmlFor="company-name">Company Name</label>
+                <input
+                  id="company-name"
+                  type="text"
+                  placeholder="Company Name"
+                  name="company-name"
+                  value=""
+                />
+                <label htmlFor="company-address">Address</label>
+                <textarea
+                  id="company-address"
+                  type="text"
+                  placeholder="Company Name"
+                  name="company-address"
+                  value=""
+                />
+                <label htmlFor="contact-name">Point of Contact</label>
+                <input
+                  id="contact-name"
+                  type="text"
+                  placeholder="Your Name"
+                  name="contact-name"
+                  value=""
+                />
+                <label htmlFor="contact-phone">Point of Contact</label>
+                <input
+                  id="contact-phone"
+                  type="tel"
+                  placeholder="Your Name"
+                  name="contact-phone"
+                  value=""
+                />
+                <label htmlFor="company-url">Company Website</label>
+                <input
+                  id="company-url"
+                  type="text"
+                  placeholder="Company Name"
+                  name="company-url"
+                  value=""
+                />
+                <label htmlFor="company-email">Email</label>
+                <input
+                  id="company-email"
+                  type="email"
+                  placeholder="preferred email"
+                  name="company-email"
+                  value=""
+                />
+                <label htmlFor="company-password">Password</label>
+                <input
+                  id="company-password"
+                  type="text"
+                  placeholder="Company Name"
+                  name="company-password"
+                  value=""
+                />
                 <Button>Sign Up</Button>
                 <p>
                   Already a member?{" "}
