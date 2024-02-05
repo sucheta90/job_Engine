1b3e0d8bec242a026260088d38f62dd24c98f20c
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2024-02-05 17:01:56 -0500

    adds a new form componentto post a job

--- client/src/pages/Dashboard/EmployerDAshboard.jsx
+++ client/src/pages/Dashboard/EmployerDAshboard.jsx
@@ -1,36 +1,45 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import JobPostForm from "../../components/Forms/JobPostForm";
 // eslint-disable-next-line no-unused-vars
 export default function EmployerDashboard(props) {
+  const [toShow, setToShow] = useState(""); // The state will be set to show the specific section on click events
   return (
     <>
       <h1>Welcome Too Your Dashboard</h1>
-      <section>
+      <section className="d-flex">
         <aside>
           <ul>
-            <li>
+            <li className="m-2">
               <Button variant="primary" size="lg">
                 Account Details
               </Button>
             </li>
-            <li>
-              <Button variant="primary" size="lg">
+            <li className="m-2">
+              <Button
+                variant="primary"
+                size="lg"
+                id="jobPost"
+                onClick={(e) => {
+                  e.target.id === "jobPost" ? setToShow("jobPost") : "";
+                }}
+              >
                 Post A Job
               </Button>
             </li>
-            <li>
+            <li className="m-2">
               <Button variant="primary" size="lg">
                 Published Jobs
               </Button>
             </li>
-            <li>
+            <li className="m-2">
               <Button variant="primary" size="lg">
                 Draft
               </Button>
             </li>
           </ul>
         </aside>
-        <div id="canvas"></div>
+        <div id="canvas">{toShow === "jobPost" && <JobPostForm />}</div>
       </section>
     </>
   );
