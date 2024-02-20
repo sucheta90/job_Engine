3476d44f029ef538563184cf000f303700808b63
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2024-02-20 17:17:27 -0500

    adds a fetch request get all posted jobs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import NavHeader from "./components/Navigation/Navbar";
 function App() {
   return (
-    <div className="main_container">
-      <NavHeader />
-      <Outlet />
-      <Footer />
-    </div>
+    <>
+      <div className="main_container">
+        <NavHeader />
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
+    </>
   );
 }
 
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import { React, useState, useEffect } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
+import axios from "axios";
 import "./Homepage.css";
+import JobDetail from "../../components/UI/JobDetail";
 
 export default function Homepage() {
   const [searchInput, setSearchInput] = useState("");
+  const [allJobs, setAllJobs] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [selectedJob, setSelectedJob] = useState("");
+
   function handleChange(e) {
     const value = e.target.value;
     setSearchInput(value);
@@ -15,58 +21,85 @@ export default function Homepage() {
     console.log(formInput.value);
     setSearchInput("");
   }
+  function handleShowDetail(e) {
+    const jobId = parseInt(
+      e.target.parentElement.parentElement.getAttribute("data-id")
+    );
+    console.log(jobId);
+    const job = allJobs.filter((job) => {
+      if (job.id === jobId) {
+        return job;
+      }
+    });
+    setSelectedJob(job[0]);
+    setShowDetails(true);
+  }
+  const handleCloseDetails = () => {
+    setShowDetails(false);
+  };
+  // Function to fetch all posted jobs
+  useEffect(() => {
+    const getAllPostedJobs = () => {
+      const jobList = axios
+        .get(`/api/jobs`)
+        .then((response) => {
+          if (!response.status === 200) {
+            throw Error({ message: "No jobs found!" });
+          }
+          setAllJobs(response.data.result);
+          return response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getAllPostedJobs();
+  }, []);
   return (
-    <Container>
-      <section className="homepage_header">
-        <h1>Welcome to the Homepage</h1>
-      </section>
-      <section className="hero">
-        <div className="hero_image_container">
-          <img
-            src="../../../assets/distance-working-abstract-concept-vector-illustration-distance-office-working-from-home-remote-job-possibility-communication-technology-online-team-meeting-digital-nomad-abstract-metaphor_335657-2923.avif"
-            alt=""
-          />
-        </div>
-        <div className="hero_form_container">
-          <form>
-            <label htmlFor="">Search a job</label>
-            <input
-              onChange={handleChange}
-              type="text"
-              id="search_input"
-              placeholder="Search by Keyword or Skill"
-              name="searchInput"
-              value={searchInput}
+    <Container className=" p-0">
+      <h1>Welcome to the Homepage</h1>
+      {showDetails ? (
+        <JobDetail job={selectedJob} handleCloseDetails={handleCloseDetails} />
+      ) : (
+        <section className="hero" style={{ float: "left" }}>
+          <div className="hero_image_container w-50">
+            <img
+              src="../../../assets/distance-working-abstract-concept-vector-illustration-distance-office-working-from-home-remote-job-possibility-communication-technology-online-team-meeting-digital-nomad-abstract-metaphor_335657-2923.avif"
+              alt=""
             />
-            {/* <input
-              onChange={handleChange}
-              type="text"
-              id="search_input"
-              placeholder="Search by Keyword or Skill"
-              name="searchInput"
-              value={searchInput}
-            />
-            <input
-              onChange={handleChange}
-              type="text"
-              id="search_input"
-              placeholder="Search by Keyword or Skill"
-              name="searchInput"
-              value={searchInput}
-            />
-            <input
-              onChange={handleChange}
-              type="text"
-              id="search_input"
-              placeholder="Search by Keyword or Skill"
-              name="searchInput"
-              value={searchInput}
-            /> */}
-          </form>
-          <Button onClick={handleFormSubmit}>Search</Button>
-        </div>
-      </section>
-      <section className=""></section>
+          </div>
+          <ul className="mt-5 w-50 p-3" style={{ overflowY: "scroll" }}>
+            {allJobs.length >= 1 ? (
+              allJobs.map((job) => {
+                return (
+                  <li key={Math.random()}>
+                    <Card
+                      className="mb-3"
+                      data-id={job.id}
+                      key={Math.random()}
+                      style={{ boxShadow: "-3px 5px 8px grey" }}
+                    >
+                      <Card.Body>
+                        <Card.Title>{job.job_title}</Card.Title>
+                        <Card.Text>{job.skills}</Card.Text>
+                        <Card.Text>
+                          {job.location_city}, {job.location_state}
+                        </Card.Text>
+                        <Card.Text>{job.job_type}</Card.Text>
+                      </Card.Body>
+                      <Card.Footer>
+                        <Button onClick={handleShowDetail}>Show Details</Button>
+                      </Card.Footer>
+                    </Card>
+                  </li>
+                );
+              })
+            ) : (
+              <h4>No jobs found</h4>
+            )}
+          </ul>
+        </section>
+      )}
     </Container>
   );
 }
